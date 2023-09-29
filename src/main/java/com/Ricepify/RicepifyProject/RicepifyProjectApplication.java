6c13f1fb42fc13265479d039c624c72ba14fa713
farahaydi <farahelaydi9@gmail.com>
2023-09-29 15:58:34 +0300

    initial commit from Spring init

---
+++ src/main/java/com/Ricepify/RicepifyProject/RicepifyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Ricepify.RicepifyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RicepifyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RicepifyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ricepify/RicepifyProject/RicepifyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ricepify.RicepifyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RicepifyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
