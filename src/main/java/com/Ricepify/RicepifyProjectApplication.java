5be9885184c5f8668e10aba445779cac90904948
Zaid Izzeldden <133864628+Zaidizz96@users.noreply.github.com>
2023-10-01 16:35:37 +0300

    fixing config

--- src/main/java/com/Ricepify/RicepifyProjectApplication.java
+++ src/main/java/com/Ricepify/RicepifyProjectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.Ricepify.RicepifyProject.Repositories")
 public class RicepifyProjectApplication {
 
 	public static void main(String[] args) {
