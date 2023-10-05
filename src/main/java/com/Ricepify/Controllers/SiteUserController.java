8387606e3924ab460862c43d55d4116c8536d487
farahisJoy <farahelaydi9@gmail.com>
2023-10-05 23:04:06 +0000

    fixing password

--- src/main/java/com/Ricepify/Controllers/SiteUserController.java
+++ src/main/java/com/Ricepify/Controllers/SiteUserController.java
@@ -8,7 +8,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.ServletException;
